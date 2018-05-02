e404a61cb15511c3a5e7de91945659f6d07ea648
Diogo Pratas <pratas@ua.pt>
2018-05-02 17:49:27 +0100

    install
    added installation file for paq compressors

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+# INSTALL PAQ BINARIES ==========================================================================
+#
+sudo apt-get install g++-multilib
+git clone https://github.com/JohannesBuchner/paq.git
+cd paq
+make
+#
+# ===============================================================================================
