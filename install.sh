352246d7fd836a362fae839dd1a91b83fccf7f01
Diogo Pratas <pratas@ua.pt>
2018-05-02 17:56:47 +0100

    minor

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ sudo apt-get install g++-multilib
 git clone https://github.com/JohannesBuchner/paq.git
 cd paq
 make
+chmod +x *.exe
 #
 # ===============================================================================================
